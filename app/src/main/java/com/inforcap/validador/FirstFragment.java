2aca05d22bcd09243ba45c2855170c632a9db8e9
crisvigorena <crisvigolara@yahoo.com>
2024-10-05 00:58:34 -0300

    commit final de rama features1, proyecto funcionando

--- app/src/main/java/com/inforcap/validador/FirstFragment.java
+++ app/src/main/java/com/inforcap/validador/FirstFragment.java
@@ -3,17 +3,25 @@ package com.inforcap.validador;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.inforcap.validador.databinding.FragmentFirstBinding;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link FirstFragment#newInstance} factory method to
+ * Use the {@link FirstFragment#} factory method to
  * create an instance of this fragment.
  */
-public class FirstFragment extends Fragment {
+/*public class FirstFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -37,7 +45,7 @@ public class FirstFragment extends Fragment {
      * @return A new instance of fragment FirstFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static FirstFragment newInstance(String param1, String param2) {
+  /*  public static FirstFragment newInstance(String param1, String param2) {
         FirstFragment fragment = new FirstFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
@@ -60,5 +68,65 @@ public class FirstFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+}*/
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+    private Button buttonValidacion;
+    private TextInputEditText password;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        binding =  FragmentFirstBinding.inflate(inflater, container, false);
+
+        View view = binding.getRoot();
+
+        buttonValidacion = binding.singIn;
+        password = binding.passInput;
+
+        buttonValidacion.setEnabled(false);
+
+        registrarListener();
+
+        return view;
+    }
+
+    private void registrarListener() {
+
+        binding.singIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String password = String.valueOf(binding.passInput.getText());
+                Toast.makeText(getContext(),"La contraseña ingresada es: " + password,Toast.LENGTH_SHORT).show();
+                Navigation.findNavController(getView()).navigate(R.id.action_firstFragment_to_secondFragment);
+            }
+        });
+
+        binding.passInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String password = charSequence.toString();
+                if (password.length() >= 5 && !password.toLowerCase().equals(password)){
+                    binding.singIn.setEnabled(true);
+                }
+                else {
+                    binding.singIn.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
     }
 }
